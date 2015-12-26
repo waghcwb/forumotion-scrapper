b9ba6c0c4632b26c757ea22d06fd67d30a5a5b76
w.souza <w.souza@fliegersoftware.de>
2015-12-26 15:42:49 -0200

    Subir código do login

---
+++ scraper/login.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from os.path import isfile
+
+class Login(object):
+	def __init__(self):
+		super(Login, self).__init__()
+
+
+	def isLogged(self):
+		return False
+
+	def check(self):
+		filename = 'account.info'
+
+		if not isfile(filename):
+			username = raw_input('Forum owner account login:\n')
+			password = raw_input('Forum owner account password:\n')
+			save = raw_input('Store password unencrypted? [yes/no]\n')
+
+			if save.lower() == 'yes':
+				with open(filename, 'a+') as fileout:
+					fileout.write('username: %s\n' % username)
+					fileout.write('password: %s' % password)
+		else:
+			with open(filename, 'r') as file:
+				content = file.read()
+				content = content.split('\n')
+
+				username = content[0].split('username: ')[1]
+				password = content[1].split('password: ')[1]
+
+		return username, password
